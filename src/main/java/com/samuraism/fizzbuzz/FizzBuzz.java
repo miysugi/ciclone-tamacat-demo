1d0b715b27147ead7c9bf39ff702afc34e9c07c6
Miyako Sugimoto <miyako@samuraism.com>
2026-02-20 13:29:45 +0900

    fizzBuzzメソッドにJavaDocコメントを再追加

--- src/main/java/com/samuraism/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/samuraism/fizzbuzz/FizzBuzz.java
@@ -7,6 +7,17 @@ public class FizzBuzz {
         }
     }
 
+    /**
+     * 指定された整数に応じて "Fizz", "Buzz", または "FizzBuzz" を返すメソッド。
+     * 以下のルールに従って結果を決定する:
+     * - 数値が3の倍数の場合は "Fizz" を返す。
+     * - 数値が5の倍数の場合は "Buzz" を返す。
+     * - 数値が3と5両方の倍数の場合は "FizzBuzz" を返す。
+     * - それ以外の場合は数値をそのまま文字列として返す。
+     *
+     * @param number 判定を行う整数
+     * @return 条件に基づき "Fizz", "Buzz", "FizzBuzz", または数値の文字列表現を返す
+     */
     public static String fizzBuzz(int number) {
         String result = "";
 
