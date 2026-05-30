0b483f56562781d3847b05ccdb9ca4362e1dd591
Miyako Sugimoto <miyako@samuraism.com>
2026-05-30 10:00:07 +0900

    fizzBuzzメソッドの不要な空白行を削除

--- src/main/java/com/samuraism/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/samuraism/fizzbuzz/FizzBuzz.java
@@ -20,15 +20,12 @@ public class FizzBuzz {
      */
     public static String fizzBuzz(int number) {
         String result = "";
-
         if (number % 3 == 0) {
             result += "Fizz";
         }
-
         if (number % 5 == 0) {
             result += "Buzz";
         }
-
         if (number % 3 != 0 && number % 5 != 0) {
             result = String.valueOf(number);
         }
